74b9e70c004cb2e85d81b31451ea3b8cea83eed6
dromero <daniel.romero@baseclass.ch>
2013-12-18 14:27:17 +0100

    Version 0.0.1

---
+++ Baseclass.Wpf.Utilities/Behaviors/WindowBehavior.cs
@@ -0,0 +1,123 @@
+﻿namespace Baseclass.Wpf.Utilities.Behaviors
+{
+    using System.Windows;
+
+    /// <summary>
+    ///     Behavior which makes it possible to let the MainWindow flash for a specific number of times.
+    /// </summary>
+    public class WindowBehavior
+    {
+        #region Static Fields
+
+        /// <summary>
+        ///     Attached property which configures the number of times the window should flash when <see cref="FlashProperty" /> is
+        ///     set to true.
+        /// </summary>
+        public static readonly DependencyProperty FlashCountProperty = DependencyProperty.RegisterAttached(
+            "FlashCount", 
+            typeof(uint), 
+            typeof(WindowBehavior), 
+            new PropertyMetadata((uint)2));
+
+        /// <summary>
+        ///     Attached property which lets the window flash for <see cref="FlashCountProperty" /> times.
+        /// </summary>
+        public static readonly DependencyProperty FlashProperty = DependencyProperty.RegisterAttached(
+            "Flash", 
+            typeof(bool), 
+            typeof(WindowBehavior), 
+            new PropertyMetadata(default(bool), FlashPropertyChanged));
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Gets the value of the <see cref="FlashProperty"/> for a specific <see cref="UIElement"/>.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="UIElement"/> from which to get the attached property value.
+        /// </param>
+        /// <returns>
+        /// The value if the window has been set to flash.
+        /// </returns>
+        public static bool GetFlash(UIElement element)
+        {
+            return (bool)element.GetValue(FlashProperty);
+        }
+
+        /// <summary>
+        /// Gets the value of the <see cref="FlashCountProperty"/> for a specific <see cref="UIElement"/>.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="UIElement"/> from which to get the attached property value.
+        /// </param>
+        /// <returns>
+        /// The number of times the window should flash.
+        /// </returns>
+        public static uint GetFlashCount(UIElement element)
+        {
+            return (uint)element.GetValue(FlashCountProperty);
+        }
+
+        /// <summary>
+        /// Lets the MainWindow flash for <see cref="FlashCountProperty"/> number of times.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="UIElement"/> from which to get the attached property value.
+        /// </param>
+        /// <param name="value">
+        /// <c>true</c> if the window should flash for a moment.
+        /// </param>
+        public static void SetFlash(UIElement element, bool value)
+        {
+            element.SetValue(FlashProperty, value);
+        }
+
+        /// <summary>
+        /// Sets the number of times the window should flash.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="UIElement"/> from which to get the attached property value.
+        /// </param>
+        /// <param name="value">
+        /// Number of times the window should flash.
+        /// </param>
+        public static void SetFlashCount(UIElement element, uint value)
+        {
+            element.SetValue(FlashCountProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Delegate which handles the changes of the <see cref="FlashProperty"/>.
+        ///     Makes the window flash if set to true, stops it otherwise.
+        /// </summary>
+        /// <param name="dependencyObject">
+        /// The element which has the property attached to it.
+        /// </param>
+        /// <param name="dependencyPropertyChangedEventArgs">
+        /// Provides data for various property changed events.
+        /// </param>
+        private static void FlashPropertyChanged(
+            DependencyObject dependencyObject, 
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if ((bool)dependencyPropertyChangedEventArgs.NewValue)
+            {
+                uint flashCount = GetFlashCount((UIElement)dependencyObject);
+
+                Application.Current.MainWindow.FlashWindow(flashCount);
+            }
+            else
+            {
+                Application.Current.MainWindow.StopFlashingWindow();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Baseclass.Wpf.Utilities/WindowExtensions.cs
@@ -0,0 +1,131 @@
+﻿namespace Baseclass.Wpf.Utilities
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Windows;
+    using System.Windows.Interop;
+
+    /// <summary>
+    /// Helper to make window flash using FlashWindowEx from user32.dll
+    /// </summary>
+    public static class WindowExtensions
+    {
+        #region Constants
+
+        /// <summary>
+        /// Flash both the window caption and taskbar button.
+        /// </summary>
+        private const uint FlashwAll = 3;
+
+        /// <summary>
+        /// Stop flashing. The system restores the window to its original state.
+        /// </summary>
+        private const uint FlashwStop = 0;
+
+        /// <summary>
+        /// Flash continuously, until the FLASHW_STOP flag is set.
+        /// </summary>
+        private const uint FlashwTimer = 4;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Lets the window flash for a specific amount of times if it's not active.
+        /// </summary>
+        /// <param name="win">
+        /// The window which should flash.
+        /// </param>
+        /// <param name="count">
+        /// The number of times the window should flash.
+        /// </param>
+        public static void FlashWindow(this Window win, uint count = uint.MaxValue)
+        {
+            // Don't flash if the window is active
+            if (win.IsActive)
+            {
+                return;
+            }
+
+            var h = new WindowInteropHelper(win);
+
+            var info = new Flashwinfo
+                           {
+                               hwnd = h.Handle, 
+                               dwFlags = FlashwAll | FlashwTimer, 
+                               uCount = count, 
+                               dwTimeout = 0
+                           };
+
+            info.cbSize = Convert.ToUInt32(Marshal.SizeOf(info));
+
+            FlashWindowEx(ref info);
+        }
+
+        /// <summary>
+        /// The stop flashing window.
+        /// </summary>
+        /// <param name="win">
+        /// The win.
+        /// </param>
+        public static void StopFlashingWindow(this Window win)
+        {
+            var h = new WindowInteropHelper(win);
+
+            var info = new Flashwinfo
+                           {
+                               hwnd = h.Handle, 
+                               dwFlags = FlashwStop, 
+                               uCount = uint.MaxValue, 
+                               dwTimeout = 0
+                           };
+
+            info.cbSize = Convert.ToUInt32(Marshal.SizeOf(info));
+            
+            FlashWindowEx(ref info);
+        }
+
+        #endregion
+
+        #region Methods
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FlashWindowEx(ref Flashwinfo pwfi);
+
+        #endregion
+
+        /// <summary>
+        /// The FlashwInfo struct
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Flashwinfo
+        {
+            /// <summary>
+            /// The size of the structure in bytes.
+            /// </summary>
+            public uint cbSize;
+
+            /// <summary>
+            /// A Handle to the Window to be Flashed. The window can be either opened or minimized.
+            /// </summary>
+            public IntPtr hwnd;
+
+            /// <summary>
+            /// The Flash Status.
+            /// </summary>
+            public uint dwFlags;
+
+            /// <summary>
+            /// Number of times to flash the window
+            /// </summary>
+            public uint uCount;
+
+            /// <summary>
+            /// The rate at which the Window is to be flashed, in milliseconds. If Zero, the function uses the default cursor blink rate.
+            /// </summary>
+            public uint dwTimeout;
+        }
+    }
+}
